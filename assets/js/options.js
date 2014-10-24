dc2a0691eb6ad7cdb8cecc7b7250e4f8d2b4f5c7
Wouter0100 <wouter@wouter0100.nl>
2014-10-24 21:35:20 +0200

    Removed options page thingies..

--- assets/js/options.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-});
-
-function showError(message) {
-    $('.alert-danger').stop().fadeOut(function() {
-       $(this).html('What did you tried? ' + message).fadeIn().delay(20000).fadeOut();
-    });
-}
