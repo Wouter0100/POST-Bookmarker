d76cb00f7a1d7d7050a5c61f86bb4573400663c1
Wouter0100 <wouter@wouter0100.nl>
2014-10-24 23:46:02 +0200

    Fixed favicon not displaying correctly.

--- redirect.php
+++ redirect.php
@@ -1,17 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page redirection</title>
+    <link rel="icon" href="https://getfavicon.appspot.com/<?php echo urldecode($_GET['url']); ?>" type="image/x-icon" />
+</head>
+<body>
+    <?php
 
-$payload = json_decode($_GET['payload']);
+    $payload = json_decode($_GET['payload']);
 
-echo '<form action="' . urldecode($_GET['url']) . '" method="POST">';
+    echo '<form action="' . urldecode($_GET['url']) . '" method="POST">';
 
-foreach ($payload as $name => $value) {
-    echo '<input type="hidden" name="' . $name . '" value="' . $value . '">';
-}
+    foreach ($payload as $name => $value) {
+        echo '<input type="hidden" name="' . $name . '" value="' . $value . '">';
+    }
 
-echo '</form>';
+    echo '</form>';
 
-?>
+    ?>
 
-<script>
-    document.getElementsByTagName('form')[0].submit();
-</script>
+    <script>
+        document.getElementsByTagName('form')[0].submit();
+    </script>
+</body>
+</html>
